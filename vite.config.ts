711dbd82291d199e2e0f63b86b508a309ade5afa
Jarbson Costa <jarbsonfc@gmail.com>
2024-12-26 22:32:20 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
         name: 'Meu App PWA',
         short_name: 'MeuApp',
